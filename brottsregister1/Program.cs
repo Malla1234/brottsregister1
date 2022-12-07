aae5d103e675fd1b9234da69b1a8e7de3f426740
Malin Lindblom <Malin.Lindblom@LAPTOP-9U8J83ML>
2022-12-07 14:52:00 +0100

    Add project files.

---
+++ brottsregister1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace brottsregister1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=LAPTOP-9U8J83ML; Initial Catalog=Brottsregister;Integrated Security=true");
+
+            SqlDataAdapter sqlDat = new SqlDataAdapter("Select * From Brott", sqlcon);
+
+            DataTable dtTbl= new DataTable();
+
+            sqlDat.Fill(dtTbl);
+
+            foreach (DataRow dr in dtTbl.Rows)
+            {
+                Console.WriteLine(dr["BrottsNamn"]);
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
